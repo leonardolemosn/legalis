5fbe58e43c28d638ff86f81ec82753ae4310da19
leonardolemosn <leonardolemosnunes@hotmail.com>
2024-03-30 22:09:44 -0300

    update

--- src/controllers/enterprise.js
+++ src/controllers/enterprise.js
@@ -10,7 +10,7 @@ const validateCNPJ_CEP = async (req, res) => {
     try {
         const cnpjExistente = await knex("users_documents").where({ document_number: cnpj }).first();
 
-        if (cnpjExistente) {
+        if (cnpjExistente && cnpjExistente.active === true) {
             return res.status(400).json({ mensagem: "Usuário já cadastrado" });
         }
 
@@ -28,7 +28,7 @@ const validateCNPJ_CEP = async (req, res) => {
         const dadosCEP = await consultarCEP(cep);
 
         if (!dadosCEP) {
-            return res.status(404).json({ error: "Dados do CNPJ não encontrados." });
+            return res.status(404).json({ error: "Dados do CEP não encontrados." });
         }
         res.json({
             dadosCNPJ,
@@ -48,12 +48,52 @@ const registerCompany = async (req, res) => {
     try {
         const cnpjExistente = await knex("users_documents").where({ document_number: cnpj }).first();
 
-        if (cnpjExistente && cnpjExistente.active === true) {
-            return res.status(400).json({ mensagem: "Usuário já cadastrado" });
-        } else {
-            knex("users_documents").where({ document_number: cnpj, active: false }).delete();
+        if (cnpjExistente && !cnpjExistente.active) {
+            await knex("offices")
+                .where({ office_id: cnpjExistente.office_id })
+                .delete();
+            await knex("users_documents").where({ document_number: cnpj }).delete();
+
+        } else if (cnpjExistente) {
+            return res.status(400).json({ mensagem: "CNPJ já cadastrado para uma empresa ativa." });
         }
 
+        const [offices] = await knex("offices").insert({
+            name: office_name,
+            parent_office_id: parent_office_id || null,
+            razao_social,
+            nome_fantasia,
+            active: true
+        }).returning('office_id');
+
+        if (!offices) {
+            return res.status(404).json({ mensagem: "Erro ao adicionar empresa" });
+        }
+
+        if (!offices.office_id) {
+            return res.status(400).json({ mensagem: "Identificador do escritório é obrigatório)" });
+        }
+
+        await knex("users_documents").insert({
+            office_id: offices.office_id,
+            document_type_id: "20f53e68-22c8-4f6b-9076-d3eb2364fe9f",
+            document_number: cnpj,
+            active: true,
+            created_at: new Date(),
+            updated_at: new Date()
+        });
+
+        await knex("offices_addresses").insert({
+            cep,
+            state,
+            city,
+            neighborhood,
+            street,
+            office_id: offices.office_id,
+            number,
+            created_at: new Date(),
+            updated_at: new Date()
+        })
 
     } catch (error) {
         console.error(error);
